0b29bce1583617999c4227f415cfb77c82a94497
Peter Fritz <me@peterfritz.dev>
2022-03-26 20:14:23 -0300

    Added support for international moms

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-export function hiMom(motherName = "mom") {
-  return `Hi, ${motherName}!`;
+import intlMom from "./lang/intlMom.js";
+
+export function hiMom(motherName, motherLang = "en") {
+  if (!motherLang || !intlMom.hasOwnProperty(motherLang)) {
+    throw new Error("Language not yet supported, but hi mom anyway!");
+  }
+
+  let re = /{{([\s\S]+)}}/;
+  let greeting = intlMom[motherLang];
+  let mom = greeting.match(re)[0].slice(2, -2);
+
+  return greeting.replace(re, motherName || mom);
 }
