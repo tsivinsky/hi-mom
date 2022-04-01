ae37201b8f8b3894ec144df1dd8bf14409e17487
Chalana Nethsara <wchalanaw@gmail.com>
2022-04-01 23:04:32 +0530

    JsDoc Documentation

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,13 @@
 import intlMom from "./lang/intlMom";
 
+/**
+ * Say hi to your mom blazingly fast!
+ * @param motherName Mother's name
+ * @param motherLang Language of your mother
+ * @example
+ * import { hiMom } from "hi-mom";
+ * console.log(hiMom()); // Hi, mom!
+ */
 export function hiMom(
   motherName?: string,
   motherLang?: keyof typeof intlMom
