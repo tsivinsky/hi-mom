88f1f5dcc33bd847bb9f48d4723502460a7d8476
João Paulo Moraes <joaopaulomoraes@outlook.com>
2022-04-05 11:00:08 -0300

    Improve function params

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
-import intlMom from "./lang/intlMom.js";
+import languages from "./languages";
 
-export function hiMom(motherName, motherLang = "en") {
-  if (!motherLang || !intlMom.hasOwnProperty(motherLang)) {
+/**
+ * A blazingly fast way to say hi to your mom!
+ * @param {string} name - The mom name
+ * @param {string} [language="en"] - The language
+ * @example hiMom()
+ * // "Hi, mom!"
+ * @example hiMom("Kate")
+ * // "Hi, Kate!"
+ * @example hiMom("Maria", "pt")
+ * // "Oi, Maria!"
+ * @returns {string} A greeting to your mom in the informed language
+ */
+export function hiMom(name, language = "en") {
+  if (!language || !languages.hasOwnProperty(language)) {
     throw new Error("Language not yet supported, but hi mom anyway!");
   }
 
   const regex = /\{{2}([^}]+)\}{2}/;
-  const greeting = intlMom[motherLang];
-  const momName = greeting.split(regex)[1];
+  const greeting = languages[language];
+  const localeName = greeting.split(regex)[1];
 
-  return greeting.replace(regex, motherName || momName);
+  return greeting.replace(regex, name || localeName);
 }
 
 export function hiMoms(mothers) {
